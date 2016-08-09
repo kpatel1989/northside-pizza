9e9f617d96378f4617f91645be01a12de2b9af2f
kartik patel <katk.1989@gmail.com>
2016-08-09 23:28:48 +0530

    ui fixes for offers

--- offers.php
+++ offers.php
@@ -8,8 +8,10 @@
 <body>
 	<?php include 'templates/background.php' ?>
     <?php include 'templates/navbar.php' ?>
-    <?php include 'templates/offers.php' ?>
-    <?php include 'templates/footer.php' ?>
+    <div class="home-container">
+	    <?php include 'templates/offers.php' ?>
+	    <?php include 'templates/footer.php' ?>
+    </div>
     <?php include 'templates/modal.html' ?>
     <?php include 'templates/scripts.php' ?>
     <script type="text/javascript" src="/js/menu.js"></script>
